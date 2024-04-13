6c36ca233c41e02f0edf322e2c8c208fa7538d43
Sahil <sahilsharma86600@gmail.com>
2024-04-13 13:20:26 +0530

    minify js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const menuBar = document.querySelector(".menu-bar");
-const open = document.querySelector(".open");
-const close = document.querySelector(".close");
-const nav = document.querySelector(".nav");
-
-console.log(6);
-menuBar.addEventListener("click", () => {
-  console.log("clicked");
-  menuBar.classList.toggle("cm");
-
-  nav.classList.toggle("cn");
-});
+const menuBar=document.querySelector(".menu-bar"),open=document.querySelector(".open"),close=document.querySelector(".close"),nav=document.querySelector(".nav");console.log(6),menuBar.addEventListener("click",()=>{console.log("clicked"),menuBar.classList.toggle("cm"),nav.classList.toggle("cn")});
