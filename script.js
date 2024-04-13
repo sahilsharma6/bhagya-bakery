1ed09de7702ba9e4914f551111efd731a8c65096
Sahil <sahilsharma86600@gmail.com>
2024-04-13 19:59:20 +0530

    some change phone and title

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const menuBar=document.querySelector(".menu-bar"),open=document.querySelector(".open"),close=document.querySelector(".close"),nav=document.querySelector(".nav");console.log(6),menuBar.addEventListener("click",()=>{console.log("clicked"),menuBar.classList.toggle("cm"),nav.classList.toggle("cn")});
+const menuBar=document.querySelector(".menu-bar"),open=document.querySelector(".open"),close=document.querySelector(".close"),nav=document.querySelector(".nav");menuBar.addEventListener("click",()=>{console.log("clicked"),menuBar.classList.toggle("cm"),nav.classList.toggle("cn")});
